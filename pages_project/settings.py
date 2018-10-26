682e11f745e0ff064008f605f6538d3fefb2e794
Lingfeng Cui <lingfengcui@Lingfengs-MacBook-Pro.local>
2018-10-26 19:18:13 +0800

    for Heroku deployment

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+m&ws(og8lf75v$l3)nx%6$2ynwus2dq*j3i)rz4(kovv4eo3%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
